ba4028d52e7d056420fd97e591e768d75e060fd2
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-14 17:50:05 -0700

    trying to get other three tests passing feel like I am close

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,13 +14,13 @@ const Order = require('../lib/models/Order');
 // }
 
 // TODO: Remove this function & use the Order model
-async function getOrderById(id) {
-  const { rows } = await pool.query('SELECT * FROM orders WHERE id=$1;', [id]);
+// async function Order.getById(id) {
+//   const { rows } = await pool.query('SELECT * FROM orders WHERE id=$1;', [id]);
 
-  if (!rows[0]) return null;
+//   if (!rows[0]) return null;
 
-  return new Order(rows[0]);
-}
+//   return new Order(rows[0]);
+// }
 
 describe('refactory routes', () => {
   beforeEach(() => {
@@ -63,7 +63,7 @@ describe('refactory routes', () => {
     ]);
   });
 
-  it.only('should be able to update an order', async () => {
+  it('should be able to update an order', async () => {
     const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app)
       .patch(`/api/v1/orders/${order.id}`)
@@ -76,7 +76,7 @@ describe('refactory routes', () => {
     };
 
     expect(res.body).toEqual(expected);
-    expect(await getOrderById(order.id)).toEqual(expected);
+    expect(await Order.getById(order.id)).toEqual(expected);
   });
 
   it('should be able to delete an order', async () => {
@@ -84,6 +84,6 @@ describe('refactory routes', () => {
     const res = await request(app).delete(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
-    expect(await getOrderById(order.id)).toBeNull();
+    expect(await Order.getById(order.id)).toBeNull();
   });
 });
